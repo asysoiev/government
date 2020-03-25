461b07d3750b05c4638c23e209a79d327a23cecd
Andrii Sysoiev <andrii.sysoiev@gmail.com>
2020-03-25 20:15:49 +0200

    JDBC: populated citizen table

--- citizens/src/main/resources/data.sql
+++ citizens/src/main/resources/data.sql
@@ -1,13 +1,30 @@
+create sequence citizen_seq
+START WITH 10000
+INCREMENT BY 1
+;
+
 create table citizen
 (
-    id        long         not null,
-    uuid      varchar(36)  not null,
+    id        long         not null default citizen_seq.nextval,
+    identifier  uuid  default random_uuid(),
     name      varchar(255) not null,
     surname   varchar(255) not null,
-    parent_id long,
-    birthday  datetime,
+    birthday  date,
     gender    char         not null,
     comment   varchar(255),
     primary key (id),
-    unique (uuid)
+    unique (identifier)
 );
+
+insert into citizen(name, surname, birthday, gender)
+values ('Dworkin', 'Barimen', DATEADD('YEAR', -1000, CURRENT_DATE), 'M');
+insert into citizen(name, surname, birthday, gender)
+values ('Oberon', 'Amber', DATEADD('YEAR', -500, CURRENT_DATE), 'M');
+insert into citizen(name, surname, birthday, gender)
+values ('Faiella', 'Amber', DATEADD('YEAR', -450, CURRENT_DATE), 'F');
+insert into citizen(name, surname, birthday, gender)
+values ('Corwin', 'Amber', DATEADD('YEAR', -350, CURRENT_DATE), 'M');
+insert into citizen(name, surname, birthday, gender)
+values ('Dara', 'Chaos', DATEADD('YEAR', -300, CURRENT_DATE), 'F');
+insert into citizen(name, surname, birthday, gender)
+values ('Merlin', 'Amber', DATEADD('YEAR', -200, CURRENT_DATE), 'M');
