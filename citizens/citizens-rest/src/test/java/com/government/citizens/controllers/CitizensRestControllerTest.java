dc4e1af7cd311189a653426742848faac00d10a3
Andrii Sysoiev <andrii.sysoiev@gmail.com>
2020-04-02 19:19:28 +0300

    Testing: simple IT for GetAllCitizens resource

---
+++ citizens/citizens-rest/src/test/java/com/government/citizens/controllers/CitizensRestControllerTest.java
@@ -0,0 +1,33 @@
+package com.government.citizens.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @author Andrii Sysoiev
+ */
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CitizensRestControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testGetAllCitizens() throws Exception {
+        mockMvc.perform(get("/citizens"))
+                .andExpect(content().contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()", is(6)));
+    }
+}
